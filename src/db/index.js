54dd3f92289204fc0f65c85bb13f5cda2c052d37
Gustavo F. Silva <gustafsilva@hotmail.com>
2018-09-18 05:00:23 -0300

    Creating new function for query and handling errors

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,5 @@
 const { Pool } = require("pg");
 const { configDB } = require("./config");
-const Log = require("log");
 
 const pool = new Pool(configDB);
 
@@ -11,7 +10,19 @@ const query = (text, callback) => {
   return pool.query(text, (err, res) => {
     const duration = Date.now() - start;
 
-    console.log(`executed query '${text}' duration: ${duration}`);
+    console.log("executed simple query", {text: text, duration: duration});
+
+    callback(err, res);
+  });
+};
+
+const queryParams = (text, params, callback) => {
+  const start = Date.now();
+
+  return pool.query(text, params, (err, res) => {
+    const duration = Date.now() - start;
+
+    console.log("executed query:", {text: text, params: params, duration: duration});
     callback(err, res);
   });
 };
@@ -21,13 +32,33 @@ const getClient = (callback) => {
 
   pool.connect((err, client, done) => {
     const duration = Date.now() - start;
+    const query = client.query.bind(client);
 
-    console.log(`new client connected, duration of connection creation: ${duration}`);
+    client.query = () => {
+      client.lastQuery = query;
+      client.query.apply(client, query);
+    };
+
+    const timeout = setTimeout(() => {
+      console.error(`A client has been checked out for more than 5 seconds!`);
+      console.error(`The last executed query on this client was: ${client.lastQuery}`);
+    }, 5000);
+
+    const release = (err) => {
+      done(err);
+
+      clearTimeout(timeout);
+
+      client.query = query;
+    }
+
+    console.log("new client connected", {duration: duration});
     callback(err, client, done);
   });
 };
 
 module.exports = {
   query: query,
+  queryParams: queryParams,
   getClient: getClient
-};
+}
